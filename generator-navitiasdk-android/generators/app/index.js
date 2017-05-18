90ba31bf0ef975b566ddd68f13a02b9aefdcf5a6
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-05-18 18:21:45 +0200

    Mustache templating begins

--- generator-navitiasdk-android/generators/app/index.js
+++ generator-navitiasdk-android/generators/app/index.js
@@ -2,20 +2,27 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const fs = require('fs-extra');
+const mustache = require('mustache');
 
 module.exports = class extends Generator {
+  initializing() {
+    fs.emptyDirSync(this.destinationPath('.'));
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the excellent ' + chalk.red('generator-navitiasdk-android') + ' generator!'
     ));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = []
+    // const prompts = [{
+    //   type: 'confirm',
+    //   name: 'someAnswer',
+    //   message: 'Would you like to enable this option?',
+    //   default: true
+    // }];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
@@ -25,9 +32,17 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath('.'),
+      this.templatePath('static'),
       this.destinationPath('.')
     );
+    // this.fs.copy(
+    //   this.templatePath('dynamic/build.gradle'),
+    //   this.destinationPath('./build.gradle')
+    // );
+    var templateBuildGradle = fs.readFileSync(this.templatePath('dynamic/build.gradle.mustache'), "utf8");
+    var renderedBuildGradle = mustache.render(templateBuildGradle, {NavitiaSDKAndroidVersion: "0.0.0.0.0"})
+    fs.writeFileSync(this.destinationPath('./build.gradle'), renderedBuildGradle);
+    
   }
 
   install() {
